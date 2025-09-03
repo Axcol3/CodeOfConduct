200fbb4c76f3a71f82cc729eec7fc2fe029f6a76
Axcol3 <asirovaalina287@gmail.com>
2025-09-03 19:23:43 +0600

    Try 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            // Путь к файлу welcome.html (относительный или абсолютный)
+            File htmlFile = new File("src/main/java/org/example/public/welcome.html");
 
+            // Проверяем, поддерживается ли Desktop
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().browse(htmlFile.toURI());
+                System.out.println("Opening welcome.html in your default browser...");
+            } else {
+                System.out.println("Desktop not supported. Open the HTML file manually.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
